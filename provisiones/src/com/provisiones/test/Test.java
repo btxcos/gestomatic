cd149429fdbd7595a1143e74ef1eb0c27752dc01
provisiones-dev <glsl.devteam@gmail.com>
2013-06-25 20:34:26 +0200

    mariadb-connection 0

---
+++ provisiones/src/com/provisiones/test/Test.java
@@ -0,0 +1,19 @@
+package com.provisiones.test;
+
+import java.sql.Connection;
+
+public class Test 
+{
+	static String sClassName = Test.class.getName();
+	static boolean bEnable = true;
+
+	public static void main(String[] args) 
+	{
+		Connection conn = null;
+
+		conn = com.provisiones.dal.ConnectionManager.OpenDBConnection();
+		com.provisiones.dal.ConnectionManager.CloseDBConnection(conn);
+				
+	}
+
+}
